6b991c75b7ad7c8f90929ce0d50f00cf26a03d1d
Innocent-Mawulorm-fiadu <innocent.fiadu@gmail.com>
2020-12-06 19:28:39 +0000

    modified App.jsx

--- Frontend/rec_turn_tracker/src/App.jsx
+++ Frontend/rec_turn_tracker/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.scss";
 import { Login, Register } from "./components/applicant/index";
 import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom";
+import Togle from "./components/applicant/Togle";
 
 class App extends React.Component {
   constructor(props) {
@@ -11,63 +12,18 @@ class App extends React.Component {
     };
   }
 
-  componentDidMount() {
-    //Add .right by default
-    this.rightSide.classList.add("right");
-  }
-
-  changeState() {
-    const { isLogginActive } = this.state;
-
-    if (isLogginActive) {
-      this.rightSide.classList.remove("right");
-      this.rightSide.classList.add("left");
-    } else {
-      this.rightSide.classList.remove("left");
-      this.rightSide.classList.add("right");
-    }
-    this.setState(prevState => ({ isLogginActive: !prevState.isLogginActive }));
-  }
 
   render() {
-    const { isLogginActive } = this.state;
-    const current = isLogginActive ? "Register" : "Login";
-    const currentActive = isLogginActive ? "login" : "register";
-    return (
-      <div className="App">
-        <div className="login">
-          <div className="container" ref={ref => (this.container = ref)}>
-            {isLogginActive && (
-              <Login containerRef={ref => (this.current = ref)} />
-            )}
-            {!isLogginActive && (
-              <Register containerRef={ref => (this.current = ref)} />
-            )}
-          </div>
-          <RightSide
-            current={current}
-            currentActive={currentActive}
-            containerRef={ref => (this.rightSide = ref)}
-            onClick={this.changeState.bind(this)}
-          />
-        </div>
-      </div>
-    );
+   
+    return(
+      <Router>
+        
+      {/* <Togle /> */}
+      <Route path="/" component={Togle} exact={true}/>
+      </ Router>
+    )
+    
   }
 }
-
-const RightSide = props => {
-  return (
-    <div
-      className="right-side"
-      ref={props.containerRef}
-      onClick={props.onClick}
-    >
-      <div className="inner-container">
-        <div className="text">{props.current}</div>
-      </div>
-    </div>
-  );
-};
 
 export default App;
