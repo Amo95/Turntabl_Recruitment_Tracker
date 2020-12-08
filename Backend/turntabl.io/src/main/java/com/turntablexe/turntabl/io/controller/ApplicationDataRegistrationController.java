ada5dc319d33bd8f7536ac59dff596022e90c216
Seth Kwashie <72032068+SethKwashie@users.noreply.github.com>
2020-12-08 14:28:28 +0000

    Update ApplicationDataRegistrationController.java

--- Backend/turntabl.io/src/main/java/com/turntablexe/turntabl/io/controller/ApplicationDataRegistrationController.java
+++ Backend/turntabl.io/src/main/java/com/turntablexe/turntabl/io/controller/ApplicationDataRegistrationController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/applicants")
+@RequestMapping("/api/turntablexe/applicants")
 public class ApplicationDataRegistrationController {
     @Autowired
     private final ApplicationDataRegistrationService applicationDataRegistrationService;
@@ -21,8 +21,8 @@ public class ApplicationDataRegistrationController {
 
     //register an applicant
     @PostMapping("/register")
-    public ApplicantData registerApplicant(@RequestBody ApplicantData applicantData) throws Exception {
-        return applicationDataRegistrationService.registerApplicants(new ApplicantData(applicantData.getId(),applicantData.getEmail(),applicantData.getFirst_name(), applicantData.getMiddle_name(),applicantData.getLast_name(), applicantData.getNickname(),applicantData.getDob(),applicantData.getGender(),applicantData.getUniversity(),applicantData.getYear_of_graduation(),applicantData.getCv()));
+    public ApplicantData registerApplicant(@RequestBody Register register) throws Exception {
+        return 
     }
 
 }
