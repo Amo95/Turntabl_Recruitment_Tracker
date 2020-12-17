db2f7e8d6747f47ae1bf35ab9b052a3b367bdb6d
franklin <forsonfranklin@gmail.com>
2020-12-17 14:39:03 +0000

    updated package json file

--- Frontend/rec_turn_tracker/src/index.js
+++ Frontend/rec_turn_tracker/src/index.js
@@ -9,7 +9,7 @@ import { Provider } from 'react-redux'
 import thunk from 'redux-thunk';
 
 
-const store = createStore(AllReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const store = createStore(AllReducer, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
