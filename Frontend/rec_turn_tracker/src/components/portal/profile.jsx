576bf6414b232bd57acccb3ebff954b338f29c20
Innocent-Mawulorm-fiadu <innocent.fiadu@gmail.com>
2020-12-29 19:28:00 +0000

    profile page created for applicant

---
+++ Frontend/rec_turn_tracker/src/components/portal/profile.jsx
@@ -0,0 +1,166 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+    state = {  }
+    render() { 
+        return ( 
+        <div className ="profile-body">
+            <div class="wrap">
+
+                <div class="left">
+                    <div className="img-size">
+                        <img src="" 
+                        alt="user" width="100" />
+                    </div>
+                    <p>Passport Photo</p>
+                </div>
+
+                <div class="right">
+                    <div class="info">
+                        <h3>Personal Information</h3>
+                        <div class="info_data">
+                            <div class="data">
+                                <span>Name</span>
+                                <p>Innocent Mawulorm Fiadu</p>
+                            </div>
+                            <div class="data">
+                                <span>Known As</span>
+                                <p>Schoolboy Dyerk Q</p>
+                            </div>
+                            <div class="data">
+                                <span>Email</span>
+                                <p>InnocentMawulorm@turntabl.com</p>
+                            </div>
+                            <div class="data">
+                                <span>Phone</span>
+                                <p>+233555154869</p>
+                                <p>+233555154869</p>
+                            </div>
+                            <div class="data">
+                                <span>Gender</span>
+                                <p>Male</p>
+                            </div>
+                            <div class="data">
+                                <span>D.O.B</span>
+                                <p>20-06-1995</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="info">
+                        <h3>Home Address</h3>
+                        <div class="info_data">
+                            <div class="data">
+                                <span>Address</span>
+                                <p>turntabl GH Ltd, 2nd floor, Sonnidom House, Achimota, Mile 7, Accra</p>
+                                <p>turntabl GH Ltd, 2nd floor, Sonnidom House, Achimota, Mile 7, Accra</p>
+                            </div>
+                            <div class="data">
+                                <span>Town|City</span>
+                                <p>Achimota</p>
+                            </div>
+                            <div class="data">
+                                <span>State|Region</span>
+                                <p>Greater Accra </p> 
+                            </div>
+                            <div class="data">
+                                <span>Zip-Code</span>
+                                <p>00233</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="info">
+                        <h3>University Details</h3>
+                        <div class="info_data">
+                            <div class="data">
+                                <span>University Name</span>
+                                <p>Ho Technical university </p>
+                            </div>
+                            <div class="data">
+                                <span>Graduation Year</span>
+                                <p>2021</p>
+                            </div>
+                            <div class="data">
+                                <span>NSS Number</span>
+                                <p>NSSGHP******* </p> 
+                            </div>
+                            <div class="data">
+                                <span>SSNIT</span>
+                                <p>C03**********</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="info">
+                        <h3>Current Employment Datails</h3>
+                        <div class="info_data">
+                            <div class="data">
+                                <span>Company Name</span>
+                                <p>TURNTABL GHANA </p>
+                            </div>
+                            <div class="data">
+                                <span>Position</span>
+                                <p>Jnr. Softwar Engineer</p>
+                            </div>
+                            <div class="data">
+                                <span>Start Date</span>
+                                <p>12-12-2019 </p> 
+                            </div>
+                            <div class="data">
+                                <span>End Date</span>
+                                <p>12-06-2020</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="info">
+                        <h3>Recommended By:</h3>
+                        <div class="info_data">
+                            <div class="data">
+                                <span>Name</span>
+                                <p> Innocent Mawulorm Fiadu </p>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+            </div>
+            {/* <h4>Personal Information</h4>
+            <div className="personal-info">
+                <div className="flow">
+                <label htmlFor="name"> Name : </label>
+                <p>Innocent Mawulorm Fiadu</p>
+                </div>
+
+                <div className="flow">
+                <label htmlFor="name"> Nike Name : </label>
+                <p>HolyByDefault</p>
+                </div>
+
+                <div className="flow">
+                <label htmlFor="mail"> E-mail : </label>
+                <p> innocent.fiadu@turntabl.io</p>
+                </div>
+
+                <div className="flow">
+                <label htmlFor="name"> Gender : </label>
+                <p>Male</p>
+                </div>
+
+                <div className="flow">
+                <label htmlFor="name"> Phone : </label>
+                <p>+233555154869</p>
+                <p>+233555154869</p>
+                </div>
+
+                <div className="flow">
+                <label htmlFor="name"> Date Of Birth : </label>
+                <p>28/06/1995</p>
+                </div>
+            </div>     */}
+        </div>
+         );
+    }
+}
+ 
+export default Profile;
---
+++ Frontend/rec_turn_tracker/src/components/portal/tracker.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Tracker extends Component {
+    state = {  }
+    render() { 
+        return ( <div className="tracker-body">
+            this is tracker
+        </div> );
+    }
+}
+ 
+export default Tracker;
